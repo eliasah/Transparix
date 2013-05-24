1f8daeb64c120c7cc451f92b77fbfbda6f034867
Isabelle RICHARD <isabellerichard89@gmail.com>
2013-05-24 23:46:08 +0200

    Tracé de l'itinéraire ajouté (brouillon)

--- src/isabelle/Map.java
+++ src/isabelle/Map.java
@@ -1,4 +1,5 @@
 package isabelle;
+
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -6,6 +7,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -19,43 +21,60 @@ import javax.swing.JPanel;
  * Cette classe représente le panneau d'affichage du plan du métro.
  * 
  * @author isabelle
- *
+ * 
  */
 public class Map extends JPanel {
 
 	private Transparix parent;
-	private Hashtable<Integer,Station> stations;
-	private Hashtable<String,Line> lines;
+	private Hashtable<Integer, Station> stations;
+	private Hashtable<String, Line> lines;
+	private LinkedList<Integer> path;
+	private BufferedImage image;
 	private int width, height;
 	private final int STATION_SIZE = 5;
 
 	/**
 	 * Constructeur d'un plan de métro.
-	 * @param stations La liste des stations à afficher.
-	 * @param lines La liste des lignes à afficher.
-	 * @param width La largeur de la carte.
-	 * @param height La longueur de la carte.
+	 * 
+	 * @param stations
+	 *            La liste des stations à afficher.
+	 * @param lines
+	 *            La liste des lignes à afficher.
+	 * @param width
+	 *            La largeur de la carte.
+	 * @param height
+	 *            La longueur de la carte.
 	 */
-	public Map(Transparix p, Hashtable<Integer, Station> stations, 
+	public Map(Transparix p, Hashtable<Integer, Station> stations,
 			Hashtable<String, Line> lines, int width, int height) {
 		this.parent = p;
 		this.stations = stations;
 		this.lines = lines;
+		this.path = new LinkedList<Integer>();
 		this.width = width;
 		this.height = height;
+		image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 		this.setLayout(null);
 		this.setPreferredSize(new Dimension(width, height));
 		this.setMinimumSize(new Dimension(width, height));
 		this.setBorder(BorderFactory.createLineBorder(Color.black));
 		this.addMouseListener(new MouseListener() {
 			@Override
-			public void mouseReleased(MouseEvent arg0) {}
+			public void mouseReleased(MouseEvent arg0) {
+			}
+
 			@Override
-			public void mousePressed(MouseEvent arg0) {}
+			public void mousePressed(MouseEvent arg0) {
+			}
+
 			@Override
-			public void mouseExited(MouseEvent arg0) {}
+			public void mouseExited(MouseEvent arg0) {
+			}
+
 			@Override
-			public void mouseEntered(MouseEvent arg0) {}
+			public void mouseEntered(MouseEvent arg0) {
+			}
+
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 				// FIXME afficher le nom dans un Panel
@@ -72,34 +91,37 @@ public class Map extends JPanel {
 	@Override
 	public void paintComponent(Graphics gg) {
 		super.paintComponent(gg);
-		Graphics2D g = (Graphics2D)gg;
+		Graphics2D g = (Graphics2D) gg;
+		g.drawImage(image, 0, 0, null);
 		Dimension dim = getSize();
-		BasicStroke stroke = new BasicStroke(3);
+		BasicStroke stroke = new BasicStroke(2);
 
 		// coloration du fond de la carte
 		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, (int)dim.getWidth(), (int)dim.getHeight());
+		g.fillRect(0, 0, (int) dim.getWidth(), (int) dim.getHeight());
 
 		// itération sur l'ensemble des stations
-		Iterator<Entry<Integer, Station>> it = this.stations.entrySet().iterator();
+		Iterator<Entry<Integer, Station>> it = this.stations.entrySet()
+				.iterator();
 		while (it.hasNext()) {
 			Station s = it.next().getValue();
-			int[] coords = this.convertCoordinatesStation(s.getLatitude(), 
+			int[] coords = this.convertCoordinatesStation(s.getLatitude(),
 					s.getLongitude(), this.width, this.height);
 
 			// affichage de la station
 			g.setColor(Color.BLACK);
-			g.fillRect(coords[0]-STATION_SIZE/2, coords[1]-STATION_SIZE/2, 
-					STATION_SIZE, STATION_SIZE);
+			g.fillRect(coords[0] - STATION_SIZE / 2, coords[1] - STATION_SIZE
+					/ 2, STATION_SIZE, STATION_SIZE);
 
 			// tracé des segments reliant la station à chacun de ses voisins
 			g.setStroke(stroke);
-			LinkedList<Couple<String,Integer>> nList = s.getNeighbours();
-			for (Couple<String,Integer> c : nList) {
+			LinkedList<Couple<String, Integer>> nList = s.getNeighbours();
+			for (Couple<String, Integer> c : nList) {
 				int idSN = c.second();
 				Station sN = this.stations.get(idSN);
-				int[] coordsSN = this.convertCoordinatesStation(sN.getLatitude(), 
-						sN.getLongitude(), this.width, this.height);
+				int[] coordsSN = this.convertCoordinatesStation(
+						sN.getLatitude(), sN.getLongitude(), this.width,
+						this.height);
 				String idLN = c.first();
 				Line lN = this.lines.get(idLN);
 				Color colorLN = lN.getColor();
@@ -107,26 +129,58 @@ public class Map extends JPanel {
 				g.drawLine(coords[0], coords[1], coordsSN[0], coordsSN[1]);
 			}
 		}
+		// tracé de l'itinéraire actuellement calculé
+		g.setColor(Color.yellow);
+		stroke = new BasicStroke(5);
+		g.setStroke(stroke);
+		Station tmp = null;
+		Iterator<Integer> itP = path.iterator();
+		while (itP.hasNext()) {
+			Station s = this.stations.get(itP.next());
+			if (tmp == null) {
+				tmp = s;
+			} else {
+				int[] coords1 = this.convertCoordinatesStation(tmp.getLatitude(),
+						tmp.getLongitude(), this.width, this.height);
+				if (!itP.hasNext()) // s1 est la station d'arrivée
+					break;
+				Station s2 = this.stations.get(itP.next());
+				int[] coords2 = this.convertCoordinatesStation(
+						s2.getLatitude(), s2.getLongitude(), this.width,
+						this.height);
+				g.drawLine(coords1[0], coords1[1], coords2[0], coords2[1]);
+				tmp = s2;
+			}
+		}
+	}
+
+	public void drawPath(LinkedList<Integer> path) {
+		this.path = path;
+		this.repaint();
 	}
 
 	/**
-	 * Détermine si le point (x,y) correspond à une station et renvoie
-	 * cette dernière, ou null dans le cas contraire.
-	 * @param x L'abscisse du point.
-	 * @param y L'ordonnée du point.
-	 * @return La station sur laquelle le clic de souris a eu lieu.
-	 * Si aucune station n'est trouvée, renvoie null.
+	 * Détermine si le point (x,y) correspond à une station et renvoie cette
+	 * dernière, ou null dans le cas contraire.
+	 * 
+	 * @param x
+	 *            L'abscisse du point.
+	 * @param y
+	 *            L'ordonnée du point.
+	 * @return La station sur laquelle le clic de souris a eu lieu. Si aucune
+	 *         station n'est trouvée, renvoie null.
 	 */
 	public Station stationPressed(int x, int y) {
-		Iterator<Entry<Integer, Station>> it = this.stations.entrySet().iterator();
+		Iterator<Entry<Integer, Station>> it = this.stations.entrySet()
+				.iterator();
 		while (it.hasNext()) {
 			Station s = it.next().getValue();
-			int[] coords = this.convertCoordinatesStation(s.getLatitude(), 
+			int[] coords = this.convertCoordinatesStation(s.getLatitude(),
 					s.getLongitude(), this.width, this.height);
 			int xS = coords[0];
 			int yS = coords[1];
-			if (x >= xS-STATION_SIZE/2 && x <= xS+STATION_SIZE/2
-					&& y >= yS-STATION_SIZE/2 && y <= yS+STATION_SIZE/2) {
+			if (x >= xS - STATION_SIZE / 2 && x <= xS + STATION_SIZE / 2
+					&& y >= yS - STATION_SIZE / 2 && y <= yS + STATION_SIZE / 2) {
 				return s;
 			}
 		}
@@ -135,13 +189,18 @@ public class Map extends JPanel {
 
 	/**
 	 * Calcule les coordonnées x et y dans un plan de width par height pixels.
-	 * @param latitude La latitude du point à afficher.
-	 * @param longitude La longitude du point à afficher.
-	 * @param width La largeur de la carte.
-	 * @param height La hauteur de la carte.
+	 * 
+	 * @param latitude
+	 *            La latitude du point à afficher.
+	 * @param longitude
+	 *            La longitude du point à afficher.
+	 * @param width
+	 *            La largeur de la carte.
+	 * @param height
+	 *            La hauteur de la carte.
 	 * @return Les coordonnées x et y.
 	 */
-	public int[] convertCoordinatesStation(double latitude, double longitude, 
+	public int[] convertCoordinatesStation(double latitude, double longitude,
 			int width, int height) {
 		int[] tab = new int[2];
 		double margin = 0.01;
@@ -149,9 +208,9 @@ public class Map extends JPanel {
 		double latitudeMax = this.getLatitudeMax() + margin;
 		double longitudeMin = this.getLongitudeMin() - margin;
 		double longitudeMax = this.getLongitudeMax() + margin;
-		double x = ((latitude - latitudeMin) 
+		double x = ((latitude - latitudeMin)
 				* (1 / (latitudeMax - latitudeMin)) * height);
-		double y = (Math.abs(1 - (longitude - longitudeMin)	
+		double y = (Math.abs(1 - (longitude - longitudeMin)
 				* (1 / (longitudeMax - longitudeMin))) * width);
 		tab[0] = (int) Math.round(x);
 		tab[1] = (int) Math.round(y);
@@ -160,59 +219,71 @@ public class Map extends JPanel {
 
 	/**
 	 * Renvoie la plus grande latitude des stations.
+	 * 
 	 * @return La plus grande latitude des stations.
 	 */
 	public double getLatitudeMax() {
 		float max = Float.MIN_VALUE;
-		Iterator<Entry<Integer, Station>> it = this.stations.entrySet().iterator();
+		Iterator<Entry<Integer, Station>> it = this.stations.entrySet()
+				.iterator();
 		while (it.hasNext()) {
 			Station s = it.next().getValue();
-			if (s.getLatitude() > max) max = s.getLatitude();
+			if (s.getLatitude() > max)
+				max = s.getLatitude();
 		}
 		return max;
 	}
 
 	/**
 	 * Renvoie la plus petite latitude des stations.
+	 * 
 	 * @return La plus petite latitude des stations.
 	 */
 	public double getLatitudeMin() {
 		float min = Float.MAX_VALUE;
-		Iterator<Entry<Integer, Station>> it = this.stations.entrySet().iterator();
+		Iterator<Entry<Integer, Station>> it = this.stations.entrySet()
+				.iterator();
 		while (it.hasNext()) {
 			Station s = it.next().getValue();
-			//java.util.Iterator<Station> it = this.stations.iterator();
-			//while (it.hasNext()) {
-			//Station s = it.next();
-			if (s.getLatitude() < min) min = s.getLatitude();
+			// java.util.Iterator<Station> it = this.stations.iterator();
+			// while (it.hasNext()) {
+			// Station s = it.next();
+			if (s.getLatitude() < min)
+				min = s.getLatitude();
 		}
 		return min;
 	}
 
 	/**
 	 * Renvoie la plus grande longitude des stations.
+	 * 
 	 * @return La plus grande longitude des stations.
 	 */
 	public double getLongitudeMax() {
 		float max = Float.MIN_VALUE;
-		Iterator<Entry<Integer, Station>> it = this.stations.entrySet().iterator();
+		Iterator<Entry<Integer, Station>> it = this.stations.entrySet()
+				.iterator();
 		while (it.hasNext()) {
 			Station s = it.next().getValue();
-			if (s.getLongitude() > max) max = s.getLongitude();
+			if (s.getLongitude() > max)
+				max = s.getLongitude();
 		}
 		return max;
 	}
 
 	/**
 	 * Renvoie la plus petite longitude des stations.
+	 * 
 	 * @return La plus petite longitude des stations.
 	 */
 	public double getLongitudeMin() {
 		float min = Float.MAX_VALUE;
-		Iterator<Entry<Integer, Station>> it = this.stations.entrySet().iterator();
+		Iterator<Entry<Integer, Station>> it = this.stations.entrySet()
+				.iterator();
 		while (it.hasNext()) {
 			Station s = it.next().getValue();
-			if (s.getLongitude() < min) min = s.getLongitude();
+			if (s.getLongitude() < min)
+				min = s.getLongitude();
 		}
 		return min;
 	}
--- src/isabelle/Transparix.java
+++ src/isabelle/Transparix.java
@@ -1,4 +1,5 @@
 package isabelle;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -6,6 +7,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.Hashtable;
+import java.util.LinkedList;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -14,20 +17,23 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
+import subway.BFS;
+import subway.Graph;
+
 /**
  * TODO
  * 
  * @author isabelle
- *
+ * 
  */
 public class Transparix implements Runnable {
 
 	private final String FILE_STATIONS = "data/data_v1/stations.txt";
-	private final String FILE_LINES = "data/data_v1/lignes.txt";	
+	private final String FILE_LINES = "data/data_v1/lignes.txt";
 
-	private Hashtable<Integer,Station> stations;
-	private Hashtable<String,Line> lines;
-	
+	private Hashtable<Integer, Station> stations;
+	private Hashtable<String, Line> lines;
+
 	private JFrame frame;
 	private JMenuBar menubar;
 	private JMenu fichier;
@@ -35,19 +41,22 @@ public class Transparix implements Runnable {
 	private Map map;
 	private JPanel panel, informations;
 	private JLabel stationName;
+	private StationSelectionCombo stationSelection;
 
 	/**
 	 * Constructeur qui charge les données de l'application TransParix.
+	 * 
 	 * @throws IOException
 	 */
 	public Transparix() {
-		this.stations =	Extraction.extractStations(FILE_STATIONS); 
+		this.stations = Extraction.extractStations(FILE_STATIONS);
 		this.lines = Extraction.extractLines(FILE_LINES);
 	}
-	
+
 	/**
 	 * Retourne le label qui contient le nom de la station sur lequel
 	 * l'utilisateur a cliqué.
+	 * 
 	 * @return Le nom de la station.
 	 */
 	public JLabel getStationName() {
@@ -71,23 +80,33 @@ public class Transparix implements Runnable {
 
 		// plan de métro
 		map = new Map(this, stations, lines, 500, 500);
-		
+		// FIXME ajout d'un itinéraire bidon pour test
+		// FIXME adapter les structures de données au graphe
+		// récupérer les données à partir du graphe
+		// ou modifier la classe Graph ?
+		Graph gs = new Graph(FILE_STATIONS);
+		BFS parcours = new BFS(gs, 1953, 1793);
+		LinkedList<Integer> list = parcours.getPath();
+		map.drawPath(list);
+
 		// panel informations
+		// TODO ajouter une classe InformationsStation qui hérite de JPanel
 		informations = new JPanel();
 		informations.setPreferredSize(new Dimension(200, 500));
 		stationName = new JLabel();
 		informations.add(stationName);
-		
+
 		// panel recherche station de métro
 		// TODO
-		
+		stationSelection = new StationSelectionCombo(this.stations.values());
+
 		// panel recherche itinéraire
 		// TODO
 
 		// panel principal
-		// TODO ajouter une classe InformationsStation qui hérite de JPanel
 		panel = new JPanel();
 		panel.setLayout(new BorderLayout());
+		panel.add(stationSelection, BorderLayout.NORTH);
 		panel.add(map, BorderLayout.WEST);
 		panel.add(informations, BorderLayout.EAST);
 
